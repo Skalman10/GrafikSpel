6ae9e21fcbeda25fd8a09e9eceeddde725b940ba
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-03-26 10:42:07 +0100

    Nu går spelet att köra

--- src/Snake.java
+++ src/Snake.java
@@ -1,12 +1,22 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
 public class Snake extends Canvas implements Runnable {
-    private int width = 800;
+    private int width = 600;
     private int height = 600;
     private Thread thread;
     private BufferStrategy bs;
+    private boolean isRunning;
 
+    public Snake() {
+        JFrame frame = new JFrame("SNAKE");
+        this.setSize(width,height);
+        frame.add(this);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
     private void draw () {
         bs = getBufferStrategy();
@@ -15,9 +25,28 @@ public class Snake extends Canvas implements Runnable {
             return;
         }
     }
+    public static void main(String[] args) {
+        Snake painting = new Snake();
+        painting.start();
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void run() {
-        
+
     }
 }
