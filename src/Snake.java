421a398dfeeac5fe0711ba696dbf70f1787020c5
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-03-26 11:01:52 +0100

    Nu har jag skapat en rektangel som rör på sig

--- src/Snake.java
+++ src/Snake.java
@@ -1,13 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
-
 public class Snake extends Canvas implements Runnable {
-    private int width = 600;
-    private int height = 600;
+    private int width = 680;
+    private int height = 680;
     private Thread thread;
     private BufferStrategy bs;
     private boolean isRunning;
+    private int fps = 30;
+    private int snakeX,snakeY,snakeVX,snakeVY;
 
     public Snake() {
         JFrame frame = new JFrame("SNAKE");
@@ -16,15 +17,37 @@ public class Snake extends Canvas implements Runnable {
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-    }
 
+        snakeX = 100;
+        snakeY = 100;
+        snakeVX = 10;
+        snakeVY = 10;
+    }
+    public void update() {
+        snakeX += snakeVX;
+        snakeY += snakeVY;
+    }
     private void draw () {
         bs = getBufferStrategy();
         if(bs == null) {
             createBufferStrategy(3);
             return;
         }
+        Graphics g = bs.getDrawGraphics();
+
+        update();
+        g.setColor(Color.GREEN);
+        g.fillRect(0,0,width,height);
+        drawSnake(g,snakeX,snakeY);
+        g.dispose();
+        bs.show();
     }
+
+    private void drawSnake(Graphics g, int x, int y) {
+        g.setColor(new Color(0,128,0));
+        g.fillRect(40+x,40+y,40,40);
+    }
+
     public static void main(String[] args) {
         Snake painting = new Snake();
         painting.start();
@@ -47,6 +70,18 @@ public class Snake extends Canvas implements Runnable {
 
     @Override
     public void run() {
+        double deltaT = 1000.0/fps;
+        long lastTime = System.currentTimeMillis();
 
+        while (isRunning) {
+            long now = System.currentTimeMillis();
+            if (now-lastTime > deltaT) {
+                update();
+                draw();
+                lastTime = now;
+            }
+
+        }
+        stop();
     }
 }
